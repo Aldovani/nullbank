eacd9dc977b8bcf3364e5c03df7cdba748335eb8
Aldovani <aldovanihcosta@gmail.com>
2023-03-18 23:37:20 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+AOS.init({
+  once: true,
+});
+
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((item) => {
@@ -11,16 +15,33 @@ questions.forEach((item) => {
   );
 });
 
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
 
-
   navigation: {
-    nextEl: '.arrow-next',
-    prevEl: '.arrow-prev',
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-prev",
   },
-
-  
 });
+
+const menu = document.querySelector(".menu");
+const navigationMobile = document.querySelector(".navigation-mobile");
+const links = document.querySelectorAll(".navigation-mobile a");
+menu.addEventListener("click", () => {
+  if (menu.classList.contains("close")) {
+    navigationMobile.classList.remove("open");
+    menu.classList.remove("close");
+    return;
+  }
+  menu.classList.add("close");
+  navigationMobile.classList.add("open");
+});
+
+links.forEach((item) =>
+  item.addEventListener("click", () => {
+    menu.classList.remove("close");
+
+    navigationMobile.classList.remove("open");
+  })
+);
